b76f9f124ec53ec4bc7c36fab9d8902851123adf
kamuikatsurgi <shahkrishang11@gmail.com>
2024-04-29 12:54:21 +0530

    root helpers

--- crates/core/src/transaction.rs
+++ crates/core/src/transaction.rs
@@ -1,3 +1,4 @@
+use base16ct::lower::encode_string;
 use eyre::Result;
 use serde::{Deserialize, Serialize};
 use sha3::{Digest, Keccak256};
@@ -24,10 +25,61 @@ impl Transaction {
             .chain_update(self.r.clone())
             .chain_update(self.s.clone())
             .finalize();
-
-        // To return the hash as 0x....
-        let hash_str = format!("0x{:x}", hash);
-
-        Ok(hash_str)
+        let hash_hex = encode_string(&hash);
+        Ok(hash_hex)
     }
+}
+
+pub fn get_transaction_root(txs: &mut Transactions) -> Result<String> {
+    if txs.is_empty() {
+        let mut hasher = Keccak256::new();
+        hasher.update(String::default());
+        let hash = hasher.finalize();
+        return Ok(encode_string(&hash));
+    }
+
+    if txs.len() % 2 != 0 {
+        txs.push(txs[txs.len() - 1].clone());
+    }
+
+    let txs_hashes: Vec<String> = txs
+        .iter()
+        .map(|tx| tx.get_transaction_hash().unwrap())
+        .collect();
+
+    Ok(construct_root(txs_hashes)?)
+}
+
+pub fn construct_root(leaves: Vec<String>) -> Result<String> {
+    let mut root = String::new();
+    let mut leaves = leaves.clone();
+
+    while leaves.len() > 1 {
+        let mut nodes = leaves.clone();
+        let mut parent_nodes = Vec::<String>::new();
+        for i in 0..nodes.len() / 2 {
+            let index = 2 * i;
+            let left = nodes[index].clone();
+            let right = nodes[index + 1].clone();
+            let hash = Keccak256::new()
+                .chain_update(left)
+                .chain_update(right)
+                .finalize();
+            parent_nodes.push(encode_string(&hash));
+        }
+
+        nodes = parent_nodes;
+        if nodes.len() == 1 {
+            root = nodes[0].clone();
+            break;
+        }
+
+        if nodes.len() % 2 != 0 {
+            nodes.push(nodes[nodes.len() - 1].clone());
+        }
+
+        leaves = nodes;
+    }
+
+    Ok(root)
 }
